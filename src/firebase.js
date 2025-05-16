6bdf3d3c8a1b38d0b90823a343316452e04249b9
Galla Niranjan <135509519+zeusXtruealpha@users.noreply.github.com>
2025-05-16 20:40:45 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { getFirestore, collection, addDoc, getDocs, query, where } from "firebas
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics"; // Import isSupported
 
+//Firebase credentials
 const firebaseConfig = {
-  apiKey: "AIzaSyBV58f4P23OR5Z4yRqQkfA9Qwd8tQCmEig",
-  authDomain: "smart-city-management-6b9ec.firebaseapp.com",
-  projectId: "smart-city-management-6b9ec",
-  storageBucket: "smart-city-management-6b9ec.appspot.com",
-  messagingSenderId: "117780971854",
-  appId: "1:117780971854:web:3d642c8ebeb069a66e5e1f",
-  measurementId: "G-D8SV3QMECX"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
